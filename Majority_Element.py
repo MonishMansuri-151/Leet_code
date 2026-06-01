a8a703d964b7f0bc5e5d7158b940e7646ccbe67a
monish <monish151mansuri@gmail.com>
2026-06-01 17:27:16 +0530

    add_new_queiton

---
+++ Majority_Element.py
@@ -0,0 +1,30 @@
+# Given an array nums of size n, return the majority element.
+
+
+# The majority element is the element that appears more than ⌊n / 2⌋
+#  times. You may assume that the majority element always
+# exists in the array.
+# Example 1:
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+class Solution:
+
+    def frequncry(self, nums):
+        my_dict = {}
+        lenght = len(nums) // 2
+        print(lenght)
+        for n in nums:
+            my_dict[n] = my_dict.get(n, 0) + 1
+        print(my_dict)
+        for key, value in my_dict.items():
+            if value > lenght:
+                return key
+
+
+arr = [2, 2, 1, 1, 1, 2, 2]
+arr = sorted(arr)
+obj = Solution()
+print(obj.frequncry(arr))
---
+++ Rotate_Array.py
@@ -0,0 +1,31 @@
+# Given an integer array nums, rotate the array to the right by k steps,
+#  where k is non-negative.
+# Example 1:
+
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# 3 appproch to solve this  qution
+# 1. brutforce approch loop 0 to k and pop ele and insert satrt
+# 2. slice kth part and + remaninig part
+# tow pointer apporch to left to right
+# using two pointer approch solve this queiton
+class Solution:
+    def method(self, nums, left, right):
+        while left < right:
+            nums[right], nums[left] = nums[left], nums[right]
+            left += 1
+            right -= 1
+
+
+arr = [-1, -100, 3, 99]
+k = 2
+obj = Solution()
+obj.method(arr, len(arr) - k, len(arr) - 1)
+obj.method(arr, 0, len(arr) - k - 1)
+obj.method(arr, 0, len(arr) - 1)
+print(arr)
