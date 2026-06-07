57c1881fa2009ed289507c995b9f06f85528b765
monish <monish151mansuri@gmail.com>
2026-06-07 16:53:55 +0530

    happy_no_added

---
+++ Happy_number_202.py
@@ -0,0 +1,34 @@
+# Write an algorithm to determine if a number n is happy.
+# A happy number is a number defined by the following process:
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+# Example 1:
+# Input: n = 19
+# Output: true
+# Explanation:
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
+class Solution:
+    def Happy_no(self, n):
+        my_set = set()
+        while n != 1:
+            if n in my_set:
+                return False
+            my_set.add(n)
+            print(my_set)
+            total = 0
+            while n > 0:
+                digit = n % 10
+                total = total + digit**2
+                n = n // 10
+            n = total
+        return True
+
+
+n = 2
+obj = Solution()
+print(obj.Happy_no(n))
